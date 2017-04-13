d889aad73200ecba63825e91529fb70b3eb3b495
Felipe Pinho <felipe76857685@gmail.com>
2017-04-13 20:18:51 -0300

    Fixing expection in WebsiteAppView

--- django_landing_page/views.py
+++ django_landing_page/views.py
@@ -15,13 +15,15 @@ class WebsiteAppView(View):
         try:
             with open(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')) as f:
                 return HttpResponse(f.read())
-        except FileNotFoundError:
+        except Exception:
             logging.exception('Production build of app not found')
             return HttpResponse(
                 """
+                <h1>
                 This URL is only used when you have built the production
                 version of the app. Visit http://localhost:3000/ instead, or
                 run `yarn run build` to test the production version.
+                </h1>
                 """,
                 status=501,
             )
